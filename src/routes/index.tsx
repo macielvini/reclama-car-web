3243382405d87a2dfeeef7c8000cf21ade68f801
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-04-05 20:43:47 -0300

    feat: auth routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 
 import AppRoutes from "./app-routes";
+import { useAuth } from "../hooks/useAuth";
+import AuthRoutes from "./auth-routes";
 
 const Routes = () => {
+  const { credentials } = useAuth();
+
   return (
     <BrowserRouter>
+      <AuthRoutes />
       <AppRoutes />
     </BrowserRouter>
   );
