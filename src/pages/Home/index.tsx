c75d475012d14cbd09e477b5deb1d42f2e5da824
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-04-05 21:18:24 -0300

    feat: welcome and instructions hidden if user has logged in

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,38 @@
 import CarCard from "../../components/CarCard";
 import Container from "../../components/Container";
 import Header from "../../components/Header";
+import { useAuth } from "../../hooks/useAuth";
 import OutlineSquareButton from "./OutlineSquareButton";
 import * as TablerIcons from "@tabler/icons-react";
 
 const Home = () => {
+  const { credentials } = useAuth();
   return (
     <Container>
       <Header />
-      <section className="flex flex-col gap-close-relation text-center">
-        <p className="text-title font-bold">
-          O Reclama Car te aproxima de quem vive o carro!
-        </p>
-        <p className="text-sm ">
-          Compartilhe suas experiências e avaliações para ajudar outros
-          usuários. Ajude-nos a tornar mais transparente o processo de escolha
-          de um carro
-        </p>
-      </section>
-      <section className="flex justify-center gap-5">
-        <OutlineSquareButton path="/" icon={<TablerIcons.IconSearch />}>
-          Pesquisar carro ou fabricante
-        </OutlineSquareButton>
-        <OutlineSquareButton path="/" icon={<TablerIcons.IconMessage />}>
-          Deixar uma avaliação
-        </OutlineSquareButton>
-      </section>
+      {!credentials && (
+        <>
+          <section className="flex flex-col gap-close-relation text-center">
+            <p className="text-title font-bold">
+              O Reclama Car te aproxima de quem vive o carro!
+            </p>
+            <p className="text-sm ">
+              Compartilhe suas experiências e avaliações para ajudar outros
+              usuários. Ajude-nos a tornar mais transparente o processo de
+              escolha de um carro
+            </p>
+          </section>
+          <section className="flex justify-center gap-5">
+            <OutlineSquareButton path="/" icon={<TablerIcons.IconSearch />}>
+              Pesquisar carro ou fabricante
+            </OutlineSquareButton>
+            <OutlineSquareButton path="/" icon={<TablerIcons.IconMessage />}>
+              Deixar uma avaliação
+            </OutlineSquareButton>
+          </section>
+        </>
+      )}
+
       <section className="flex flex-col gap-relation">
         <p className="text-subtitle font-bold">Carros mais avaliados:</p>
         <div className="flex touch-pan-x snap-x gap-relation overflow-x-auto scroll-smooth">
