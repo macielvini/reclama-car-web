96c488b31fbf42a95f0c9626c511a885144ebbc2
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-04-14 21:56:14 -0300

    feat; review route

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Review from "../pages/Review";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/reviews/new" element={<Review />} />
     </Routes>
   );
 };
