3aca6220cfc05d6eb42eee48a9d71de55a3f7f47
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-04-18 20:09:06 -0300

    feat: horizontal separator component

---
+++ src/components/HorizontalSeparator.tsx
@@ -0,0 +1,10 @@
+import * as Separator from "@radix-ui/react-separator";
+
+const HorizontalSeparator = () => (
+  <Separator.Root
+    className="my-3 h-[1px] w-full bg-gray-300"
+    orientation="horizontal"
+  />
+);
+
+export default HorizontalSeparator;
